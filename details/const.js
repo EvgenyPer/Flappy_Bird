abc486311402cc4190e95581c51a777787547950
EvgenyPer <125033178+EvgenyPer@users.noreply.github.com>
2023-09-10 13:42:49 +0300

    Add files via upload

---
+++ details/const.js
@@ -0,0 +1,43 @@
+// определяем канвас и контекст
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+// определяем некоторые переменные
+let frames = 0;
+let index = 0;
+
+let SPEED = 2;
+
+let frameRate = 60; // Желаемая частота кадров (FPS)
+let frameDelay = 1000 / frameRate; // Задержка между кадрами в миллисекундах
+
+const DEGREE = Math.PI/180;
+
+// подгружаем картиночку
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+// добавлям звуки в массив
+const SOUNDS = {
+  SCORE: new Audio("audio/point.mp3"),
+  FLAP: new Audio("audio/flap.mp3"),
+  HIT: new Audio("audio/hit.mp3"),
+  SWOOSHING: new Audio("audio/swooshing.mp3"),
+  DIE: new Audio("audio/die.mp3"),
+};
+
+// состояния игры
+const state = {
+    current: 0,
+    getReady: 0,
+    game: 1,
+    over: 2
+}
+
+// коорбинаты кнопки старта
+const startBtn = {
+    x: 120,
+    y: 263,
+    w: 83,
+    h: 29
+}
